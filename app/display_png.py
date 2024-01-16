fae27323f9d4d88af1300a485bd577ac7d8ffbb2
Gustavo Abbomerato Zantut <gustavo_Zantut@hotmail.com>
2024-01-16 14:45:25 -0300

    increase buffer

--- app/display_png.py
+++ app/display_png.py
@@ -13,7 +13,7 @@ def execute_gst_command(file_path):
     command = [
         "/bin/bash",
         "-c",
-        f"gst-launch-1.0 -v filesrc location=\"{file_path}\" ! decodebin ! videoconvert ! imagefreeze num-buffers=500 ! autovideosink"
+        f"gst-launch-1.0 -v filesrc location=\"{file_path}\" ! decodebin ! videoconvert ! imagefreeze num-buffers=5000 ! autovideosink"
     ]
     subprocess.run(command)
     
